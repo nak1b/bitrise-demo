5ca1d5a41cfc0c4adb788a43d28f9be100ffe194
Nakib Momin <nbmomin14@gmail.com>
2018-01-18 17:03:04 -0500

    removed test

--- e2e/firstTest.spec.js
+++ e2e/firstTest.spec.js
@@ -1,9 +1,9 @@
-describe('Example', () => {
-  // beforeEach(async () => {
-  //   await device.reloadReactNative();
-  // });
-  //
-  // it('should have welcome screen', async () => {
-  //   await expect(element(by.id('welcome'))).toBeVisible();
-  // });
-})
+// describe('Example', () => {
+//   // beforeEach(async () => {
+//   //   await device.reloadReactNative();
+//   // });
+//   //
+//   // it('should have welcome screen', async () => {
+//   //   await expect(element(by.id('welcome'))).toBeVisible();
+//   // });
+// })
